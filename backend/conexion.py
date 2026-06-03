1efca7d7f4f702eb2b82b38e30f46b3c146f82bb
amdo19 <161642912+amdo19@users.noreply.github.com>
2026-06-02 22:27:29 -0500

    Base de datos en la nube y conexion SSL lista

--- backend/conexion.py
+++ backend/conexion.py
@@ -6,7 +6,7 @@ def obtener_conexion():
     return pymysql.connect(
         host=os.environ.get('DB_HOST', 'localhost'),
         user=os.environ.get('DB_USER', 'root'),
-        password=os.environ.get('DB_PASSWORD', ''),
+        password=os.environ.get('DB_PASSWORD', 'AVNS_hxO5F46HlM-9bcUPoY4'),
         database=os.environ.get('DB_NAME', 'sistema_sri'),
         port=int(os.environ.get('DB_PORT', 3306)),
         ssl={'ssl': {}} # ⚠️ ¡IMPORTANTE! Aiven exige conexión segura SSL como muestra tu imagen
