32b3f75058378a9537094f5c53db6922a07725f7
amdo19 <161642912+amdo19@users.noreply.github.com>
2026-06-02 22:50:38 -0500

    Forzar casteo de puerto a entero para Render

--- backend/conexion.py
+++ backend/conexion.py
@@ -2,12 +2,11 @@ import os
 import pymysql
 
 def obtener_conexion():
-    # Si está en Render, usará los datos de la nube; si estás en tu PC, usará los valores por defecto locales
     return pymysql.connect(
-        host=os.environ.get('DB_HOST', 'localhost'),
-        user=os.environ.get('DB_USER', 'root'),
-        password=os.environ.get('DB_PASSWORD', 'AVNS_hxO5F46HlM-9bcUPoY4'),
-        database=os.environ.get('DB_NAME', 'sri'),
-        port=int(os.environ.get('DB_PORT', 3306)),
-        ssl={'ssl': {}} # ⚠️ ¡IMPORTANTE! Aiven exige conexión segura SSL como muestra tu imagen
+        host=os.environ.get('DB_HOST'),
+        port=int(os.environ.get('DB_PORT', 21545)), # ⬅️ El int() es obligatorio aquí para la nube
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        database=os.environ.get('DB_NAME'),
+        ssl={'ssl': {}} # Conexión segura para Aiven
     )
