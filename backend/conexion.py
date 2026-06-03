c0bb93757fb05f503160f4aff5013ab2b1463464
amdo19 <161642912+amdo19@users.noreply.github.com>
2026-06-02 22:10:13 -0500

    Ajuste de conexion para variables de entorno con SSL

--- backend/conexion.py
+++ backend/conexion.py
@@ -1,11 +1,13 @@
+import os
 import pymysql
 
 def obtener_conexion():
-    # Configuración por defecto para XAMPP / phpMyAdmin
+    # Si está en Render, usará los datos de la nube; si estás en tu PC, usará los valores por defecto locales
     return pymysql.connect(
-        host='localhost',
-        user='root',       # Usuario predeterminado de XAMPP
-        password='',       # Contraseña en blanco por defecto en XAMPP
-        db='sistema_sri',  # El nombre de la base de datos que creaste
-        cursorclass=pymysql.cursors.DictCursor
+        host=os.environ.get('DB_HOST', 'localhost'),
+        user=os.environ.get('DB_USER', 'root'),
+        password=os.environ.get('DB_PASSWORD', ''),
+        database=os.environ.get('DB_NAME', 'sistema_sri'),
+        port=int(os.environ.get('DB_PORT', 3306)),
+        ssl={'ssl': {}} # ⚠️ ¡IMPORTANTE! Aiven exige conexión segura SSL como muestra tu imagen
     )
